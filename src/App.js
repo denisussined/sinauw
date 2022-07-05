77351c99c7217dcc58a1b534db07eabc63305359
Denis Sidletskyi <denis.sidletskyy@fivesysdev.com>
2022-07-05 13:54:17 +0300

    Added fonts files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import app from './App.module.sass';
+import Header from "./components/header/Header";
 
 const App = () => {
-  return (
-      <div className={app.wrapper}>
-        app
-      </div>
-  )
+    return (
+        <div className={app.wrapper}>
+            <Header />
+        </div>
+    )
 }
 
 export default App;
