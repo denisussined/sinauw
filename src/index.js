19d8ac5988ae24743b3a22f91180b782d9e7fb9e
Denis Sidletskyi <denis.sidletskyy@fivesysdev.com>
2022-07-08 11:45:12 +0300

    SD/fix/header

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import './fonts/Inter-Regular.ttf'
+import './fonts/Inter.ttf'
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
