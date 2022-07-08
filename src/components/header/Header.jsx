553e62a7948796db7fd212f6ac9b30077fde410a
Denis Sidletskyi <denis.sidletskyy@fivesysdev.com>
2022-07-08 11:45:12 +0300

    SD/fix/header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,28 +1,28 @@
-import header from './Header.module.sass'
+import s from './Header.module.sass'
 import logo from "../../images/header_logo.svg"
 import lang from "../../images/header_language.svg"
 
 const Header = () => {
     return (
-        <div className={header.wrapper}>
+        <div className={s.wrapper}>
 
-            <div className={header.logo}>
+            <div className={s.logo}>
                 <img src={logo} alt="logo"/>
             </div>
 
-            <nav className={header.navbar}>
-                <span>Home</span>
-                <span>Courses</span>
-                <span>Webinar</span>
-                <span>Pricing</span>
+            <nav className={s.navbar}>
+                <span className={s.navbar__item}>Home</span>
+                <span className={s.navbar__item}>Courses</span>
+                <span className={s.navbar__item}>Webinar</span>
+                <span className={s.navbar__item}>Pricing</span>
             </nav>
 
-            <div className={header.language}>
-                <img src={lang} alt="language"/>
+            <div className={s.language}>
+                <img className={s.language__image} src={lang} alt="language"/>
             </div>
 
-            <div className={header.login}>
-                <button>Login</button>
+            <div className={s.login}>
+                <button className={s.login__button}>Login</button>
             </div>
 
         </div>
