125edffe42556def46be15bab0009a45554b7602
Denis Sidletskyi <denis.sidletskyy@fivesysdev.com>
2022-07-14 13:07:52 +0300

    DS/task/Mobile section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import s from './App.module.sass';
 import Header from "./components/header/Header";
 import Hero from "./components/hero/Hero";
+import Mobile from "./components/mobile/Mobile";
 
 const App = () => {
     return (
@@ -8,6 +9,7 @@ const App = () => {
             <div className={s.wrapper}>
                 <Header />
                 <Hero />
+                <Mobile />
             </div>
         </div>
     )
