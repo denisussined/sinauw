d3e378c7c687d0d7373b977884db85ab4bb7d38e
Denis Sidletskyi <denis.sidletskyy@fivesysdev.com>
2022-07-19 13:53:55 +0300

    DS/task/Classes section

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Hero from "./components/hero/Hero";
 import Featured from "./components/featured/Featured";
 import Mobile from "./components/mobile/Mobile";
 import Footer from "./components/footer/Footer";
+import Classes from "./components/classes/Classes";
 
 const App = () => {
     return (
@@ -12,6 +13,7 @@ const App = () => {
                 <Header />
                 <Hero />
                 <Featured />
+                <Classes />
                 <Mobile />
                 <Footer />
             </div>
